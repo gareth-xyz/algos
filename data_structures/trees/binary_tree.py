14a298f1b187617587e80e445fd788110fe6367e
garethiv <gareth@garethveale.dev>
2021-09-22 16:24:58 +0100

    insert, traversal and demo code

--- data_structures/trees/binary_tree.py
+++ data_structures/trees/binary_tree.py
@@ -40,12 +40,23 @@ class BinaryTree:
 
     def insert(self, elem):
         """ inserts a node or element into the tree """
-
         if self.root is None:
-            if isinstance(item, Node):
-                self.root = elem
-            else:
-                self.root = Node(elem)
+            self.root = Node(elem)
+        else:
+            queue = deque()
+            queue.append(self.root)
+
+            while len(queue):
+                temp = queue.popleft()
+
+                if not temp.left:
+                    temp.left = Node(elem)
+                    break
+                if not temp.right:
+                    temp.right = Node(elem)
+                    break
+                queue.append(temp.left)
+                queue.append(temp.right)
 
     def delete(self, root, elem):
         """ removes an element from the tree - replaces with bottom-right most node """
@@ -55,7 +66,11 @@ class BinaryTree:
         else:
             return False
 
-    def search(self, root, elem):
+    def search(self, elem):
+        """ public function to search for an element in the tree """
+        return self._search(self.root, elem)
+
+    def _search(self, root, elem):
         """ searches for the element in the tree by checking each node's val attribute """
         if root is None:
             return False
@@ -63,24 +78,24 @@ class BinaryTree:
         if root.val == elem:
             return True
 
-        self.search(root.left, elem)
-        self.search(root.right, elem)
+        self._search(root.left, elem)
+        self._search(root.right, elem)
 
-    def traverse(self, root, traverse_type):
+    def traverse(self, traverse_type):
         """ traverses the tree by the given type """
-        if traverse_type = TreeTraversalOrder.PRE_ORDER:
-            return self._pre_order_traversal(root)
-        if traverse_type = TreeTraversalOrder.IN_ORDER:
-            return self._in_order_traversal(root)
-        if traverse_type = TreeTraversalOrder.POST_ORDER:
-            return self._post_order_traversal(root)
-        if traverse_type = TreeTraversalOrder.LEVEL_ORDER:
-            return self._level_order_traversal(root)
+        if traverse_type == TreeTraversalOrder.PRE_ORDER:
+            return self._pre_order_traversal(self.root)
+        if traverse_type == TreeTraversalOrder.IN_ORDER:
+            return self._in_order_traversal(self.root)
+        if traverse_type == TreeTraversalOrder.POST_ORDER:
+            return self._post_order_traversal(self.root)
+        if traverse_type == TreeTraversalOrder.LEVEL_ORDER:
+            return self._level_order_traversal(self.root)
 
     def _pre_order_traversal(self, root):
         """ performs pre-order traversal, root>left>right """
         if root is None:
-            return False
+            return
         else:
             print(root.val)
             self._pre_order_traversal(root.left)
@@ -89,7 +104,7 @@ class BinaryTree:
     def _in_order_traversal(self, root):
         """ performs in-order traversal, left>root>right """
         if root is None:
-            return False
+            return
         else:
             self._pre_order_traversal(root.left)
             print(root.val)
@@ -98,7 +113,7 @@ class BinaryTree:
     def _post_order_traversal(self, root):
         """ performs post-order traversal, right>left>root """
         if root is None:
-            return False
+            return
         else:
             self._pre_order_traversal(root.left)
             self._pre_order_traversal(root.right)
@@ -126,7 +141,17 @@ class BinaryTree:
 
 def main():
     binary_tree = BinaryTree()
-
+    binary_tree.root = Node(1)
+    binary_tree.root.left = Node(2)
+    binary_tree.root.right = Node(3)
+    binary_tree.root.left.left = Node(4)
+    binary_tree.root.left.right = Node(5)
+    binary_tree.root.right.left = Node(6)
+    binary_tree.root.right.right = Node(7)
+    binary_tree.traverse( TreeTraversalOrder.LEVEL_ORDER )
+    binary_tree.search(7)
+    binary_tree.insert(8)
+    binary_tree.traverse( TreeTraversalOrder.LEVEL_ORDER )
 
 if __name__ == '__main__':
     main()
