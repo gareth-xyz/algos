c22f09c1bdff25fd4374a1975fdf7d1e3bee0995
garethiv <gareth@garethveale.dev>
2021-09-27 17:23:02 +0100

    finished BST methods

--- data_structures/trees/binary_search_tree.py
+++ data_structures/trees/binary_search_tree.py
@@ -38,12 +38,81 @@ class BinarySearchTree:
         self.root = root
 
     def insert(self, elem):
-        """ inserts a node or element into the tree """
-        pass
+        """ inserts an element into the tree, returns True if successful """
+        if self.search(elem):
+            return False
+        
+        self.root = self._insert(self.root, elem)
+        return True
+
+    def _insert(self, node, elem):
+        """ recursive private method to insert into the tree """
+        if node == None:
+            node = Node(None, None, elem)
+        
+        else:
+            if elem < node.val:
+                node.left = self._insert(node.left, elem)
+            else:
+                node.right = self._insert(node.right, elem)
+
+        return node
 
     def delete(self, elem):
-        """ removes an element from the tree - replaces with bottom-right most node """
-        pass
+        """ removes an element from the tree if it exists, returns True if successful """
+        if self.search(elem):
+            self.root = self._delete(self.root, elem)
+            return True
+        else:
+            return False
+
+    def _delete(self, node, elem):
+        """ recursive private method to delete an element from the tree """
+        if node == None:
+            return
+        # Cases:
+            # 1 remove leaf node - simply remove
+            # 2 remove a node with one child - replace with left/right node
+            # 3 remove a node with 2 children - replace with either largest in left/smallest in right
+                # if left:  go as far right as possible in left child
+                # if right: go as far left as possible in right child
+        
+        if elem == node.data:
+            
+            # case 1 or 2
+            if not node.left:
+                right_child = node.right
+                node.val = None
+                node = None
+                return right_child
+
+            # case 2
+            elif not node.right:
+                left_child = node.left
+                node.val = None
+                node = None
+                return left_child
+
+            # case 3
+            # if both left & right, replace target with min node in right subtree, then delete
+            else:
+                # find min in right
+                temp = node.right
+                while temp.left:
+                    temp = temp.left
+                # swap data    
+                node.data = temp.val
+                # remove the node we swapped with
+                node.right = self._remove(node.right, temp.data)
+            
+        elif elem < node.val:
+            node.left = self._delete(node.left, elem)
+        elif elem > node.val:
+            node.right = self._delete(node.right, elem)
+        else:
+            return
+
+        return node
 
     def search(self, elem):
         """ public function to search for an element in the tree """
@@ -51,15 +120,17 @@ class BinarySearchTree:
 
     def _search(self, root, elem):
         """ searches for the element in the tree by checking each node's val attribute """
-        #TODO - modify for BST
         if root is None:
             return False
         
         if root.val == elem:
             return True
+        
         else:
-            self._search(root.left, elem)
-            self._search(root.right, elem)
+            if elem < root.val:
+                self._search(root.left, elem)
+            else:
+                self._search(root.right, elem)
 
     def traverse(self, traverse_type):
         """ traverses the tree by the given type """
