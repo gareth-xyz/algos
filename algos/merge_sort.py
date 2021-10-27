26a080ec2ed39344e0cb5147aeaff7bfa27db0f3
garethiv <gareth@garethveale.dev>
2021-10-27 10:16:33 +0100

    merge and quick sort modules

---
+++ algos/merge_sort.py
@@ -0,0 +1,7 @@
+
+def merge_sort(arr):
+    pass
+
+if __name__ == '__main__':
+    arr = [6,3,4,5,9]
+    merge_sort(arr)
---
+++ algos/quick_sort.py
@@ -0,0 +1,7 @@
+
+def quick_sort(arr):
+    pass
+
+if __name__ == '__main__':
+    arr = [6,3,4,5,9]
+    quick_sort(arr)
