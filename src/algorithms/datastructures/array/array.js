5b38038524b20136af98bacfe3db2ec459b7cd2c
garethiv <gareth@garethveale.dev>
2022-01-13 17:30:56 +0000

    array in js

---
+++ src/algorithms/datastructures/array/array.js
@@ -0,0 +1,10 @@
+/**
+ * As with Python this doesn't really make sense but 
+ * i played around with the array objet nonetheless.
+ */
+
+const myArray = [1,2,3,4,5,6,7,8,9,10]
+const myStringArray = ['hello','world']
+
+console.log(myArray)
+console.log(myStringArray)
